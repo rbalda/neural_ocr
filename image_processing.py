5422adc67791aabe01998bba0ff3a576f6569100
Rene Balda <rabt89@gmail.com>
2016-01-13 19:44:04 -0500

    crop digits

--- image_processing.py
+++ image_processing.py
@@ -1,5 +1,11 @@
 import cv2
 import numpy as np
+import sys
+WINDOW_SIZE = 20
+ROWS = 0
+COLUMNS = 1
+
+
 
 def binarize_and_filter(img):
     out = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -8,12 +14,39 @@ def binarize_and_filter(img):
     return out
 
 
+def resize(img):
+    out = cv2.resize(img,(WINDOW_SIZE,WINDOW_SIZE),interpolation=cv2.INTER_CUBIC)
+    return out
 
-#def crop_image(image):
+def crop_digits(input_img):
+    rows = input_img[ROWS]
+    columns = input_img[COLUMNS]
+    tly,tlx,bry,brx = 0
+    width , height = 0
+    suml, sumr = 0
+    flag = 0;
+    for i in range(1,rows):
+        for j in range(1,columns):
+            if input_img[i,j]==0:
+                flag = 1
+                tly = i
+                break
+        if flag == 1:
+            flag = 0
+            break
+
+
+
+
+
+
+
+
 
 img = cv2.imread("digitrecognition1.jpg")
 cv2.imshow("normal",img)
 img = binarize_and_filter(img)
+img = resize(img)
 cv2.imshow("hello",img)
 cv2.waitKey()
 #binarize_and_filter(img)

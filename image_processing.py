8c9892a23a3331f0c4df691c96953cea054d47b9
Rene Balda <rbalda@espol.edu.ec>
2016-01-15 10:09:39 -0500

    upload project

--- image_processing.py
+++ image_processing.py
@@ -1,16 +1,31 @@
 import cv2
 import numpy as np
 import sys
+from numpy import *
+from pybrain.structure import *
+from pybrain.datasets import SupervisedDataSet
+from pybrain.utilities import percentError
+from pybrain.supervised.trainers import BackpropTrainer
+import learning
+
 WINDOW_SIZE = 20
 ROWS = 0
 COLUMNS = 1
+N_INPUT_LAYER = WINDOW_SIZE*WINDOW_SIZE
+N_HIDDEN_LAYER = WINDOW_SIZE/2
+OUTPUT_LAYER = 10
+input_layer = LinearLayer(N_INPUT_LAYER)
+hidden_layer = SigmoidLayer(N_HIDDEN_LAYER)
+output_layer = SoftmaxLayer(OUTPUT_LAYER)
+net = None
+
 
 
 
 def binarize_and_filter(img):
     out = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     out = cv2.GaussianBlur(out,(3,3),0)
-    #ret,out = cv2.threshold(out,150,255,cv2.THRESH_BINARY_INV)
+    ret,out = cv2.threshold(out,200,255,cv2.THRESH_BINARY)
     return out
 
 
@@ -18,16 +33,21 @@ def resize(img):
     out = cv2.resize(img,(WINDOW_SIZE,WINDOW_SIZE),interpolation=cv2.INTER_CUBIC)
     return out
 
-def crop_digits(input_img):
-    rows = input_img[ROWS]
-    columns = input_img[COLUMNS]
-    tly,tlx,bry,brx = 0
-    width , height = 0
-    suml, sumr = 0
-    flag = 0;
-    for i in range(1,rows):
-        for j in range(1,columns):
-            if input_img[i,j]==0:
+def crop_image(input_img):
+    img_height = input_img.shape[ROWS]
+    img_width = input_img.shape[COLUMNS]
+    tly=None
+    tlx=None
+    bry=None
+    brx = None
+    width=None
+    height = None
+
+    flag = 0
+    """"margen superior"""
+    for i in range(0,img_height):
+        for j in range(0,img_width):
+            if input_img[i][j]==0:
                 flag = 1
                 tly = i
                 break
@@ -35,18 +55,104 @@ def crop_digits(input_img):
             flag = 0
             break
 
+    """margen inferior"""
+    for i in range(img_height-1,0,-1):
+        for j in range(0,img_width):
+            if input_img[i][j]==0:
+                flag = 1
+                bry = i
+                break
+        if flag == 1:
+            flag = 0
+            break
+
+    """margen izquierdo"""
+    for j in range(0,img_width):
+        for i in range(0,img_height-1):
+            if input_img[i][j]==0:
+                flag=1
+                tlx = j
+                break
+        if flag==1:
+            flag = 0
+            break
+
+
+    """margen derecho"""
+    for j in range(img_width-1,0,-1):
+        for i in range(0,img_height-1):
+            if input_img[i,j]==0:
+                flag = 1
+                brx = j
+                break
+        if flag==1:
+            flag = 0
+            break
+
+    width = brx - tlx
+    height = bry - tly
+    croped = input_img[tly:tly+height,tlx:tlx+width]
+    return croped
+
+
+
+def generate_pattern(img):
+    pixel_array = []
+    i = 0
+    for x in range(0,WINDOW_SIZE):
+        for y in range(0,WINDOW_SIZE):
+            if img[x,y]==255:
+                pixel_array.append(1)
+            else:
+                pixel_array.append(0)
+    pixel_array = array(pixel_array)
+    # pixel_array.dtype = np.uint8
+    return pixel_array
+
+
+def print_patern(x):
+    for i in range(0,WINDOW_SIZE*WINDOW_SIZE):
+        if i % WINDOW_SIZE == 0:
+            print "\n"
+        else:
+            if x[i] == 1:
+                print "x"
+            else:
+                print "-"
+
+
+def init_neural_network():
+    neural_net = FeedForwardNetwork()
+    neural_net.addInputModule(input_layer)
+    neural_net.addModule(hidden_layer)
+    neural_net.addOutputModule(output_layer)
+    connect1 = FullConnection(input_layer,hidden_layer)
+    connect2 = FullConnection(hidden_layer,output_layer)
+    neural_net.addConnection(connect1)
+    neural_net.addConnection(connect2)
+    neural_net.sortModules()
+    return neural_net
 
 
 
 
 
-
-
-
-img = cv2.imread("digitrecognition1.jpg")
-cv2.imshow("normal",img)
+img = cv2.imread("number.png")
+# cv2.imshow("normal",img)
 img = binarize_and_filter(img)
+img = crop_image(img)
 img = resize(img)
-cv2.imshow("hello",img)
-cv2.waitKey()
+ablob = generate_pattern(img)
+# net = init_neural_network()
+training = learning.get_labeled_data('train-images-idx3-ubyte.gz',
+                                'train-labels-idx1-ubyte.gz', 'training')
+blob = training['x'][9]
+print blob.shape
+learning.view_image(ablob)
+# print net
+# print_patern(a)
+# cv2.imshow("hello",img)
+# cv2.imshow("blob",blob)
+# cv2.waitKey()
+
 #binarize_and_filter(img)
---
+++ learning.py
@@ -0,0 +1,67 @@
+import cv2
+
+__author__ = 'rbalda'
+from struct import unpack
+import gzip
+import matplotlib.pylab as plt
+from numpy import zeros, uint8
+import pylab
+import os.path
+import cPickle as pickle
+
+def get_labeled_data(imagefile, labelfile, picklename):
+    """Read input-vector (image) and target class (label, 0-9) and return
+       it as list of tuples.
+    """
+    if os.path.isfile('%s.pickle' % picklename):
+        data = pickle.load(open('%s.pickle' % picklename))
+    else:
+        # Open the images with gzip in read binary mode
+        images = gzip.open(imagefile, 'rb')
+        labels = gzip.open(labelfile, 'rb')
+
+        # Read the binary data
+
+        # We have to get big endian unsigned int. So we need '>I'
+
+        # Get metadata for images
+        images.read(4)  # skip the magic_number
+        number_of_images = images.read(4)
+        number_of_images = unpack('>I', number_of_images)[0]
+        rows = images.read(4)
+        rows = unpack('>I', rows)[0]
+        cols = images.read(4)
+        cols = unpack('>I', cols)[0]
+
+        # Get metadata for labels
+        labels.read(4)  # skip the magic_number
+        N = labels.read(4)
+        N = unpack('>I', N)[0]
+
+        if number_of_images != N:
+            raise Exception('The number of labels did not match '
+                            'the number of images.')
+
+        # Get the data
+        x = zeros((N, rows, cols), dtype=uint8)  # Initialize numpy array
+        y = zeros((N, 1), dtype=uint8)  # Initialize numpy array
+        for i in range(N):
+            if i % 1000 == 0:
+                print("i: %i" % i)
+            for row in range(rows):
+                for col in range(cols):
+                    tmp_pixel = images.read(1)  # Just a single byte
+                    tmp_pixel = unpack('>B', tmp_pixel)[0]
+                    x[i][row][col] = (float(tmp_pixel) / 255)
+            tmp_label = labels.read(1)
+            y[i] = unpack('>B', tmp_label)[0]
+        data = {'x': x, 'y': y, 'rows': rows, 'cols': cols}
+        pickle.dump(data, open("%s.pickle" % picklename, "wb"))
+    return data
+
+
+def view_image(image, label=""):
+    """View a single image."""
+    print("Label: %s" % label)
+    plt.imshow(image,cmap=plt.cm.gray)
+    plt.show()
